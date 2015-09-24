e5aaf7c175fc051516219853b61f0ad472b68d70
Tomoumi NISHIMURA <lue.gsk.2012@gmail.com>
2015-09-24 16:42:30 +0900

    bugfix: knockoff

--- app/me/nsmr/parser/MarkdownParser.scala
+++ app/me/nsmr/parser/MarkdownParser.scala
@@ -18,15 +18,49 @@ import com.tristanhunt.knockoff.UnorderedList
 import com.tristanhunt.knockoff.UnorderedItem
 
 class GFChunkParser extends ChunkParser {
+  import com.tristanhunt.knockoff._
+
   override lazy val indentedChunk: Parser[Chunk] =
     rep1(indentedLine) ^^ (lines => IndentedChunk(foldedString(lines))) |
     { lazy val _triple: Parser[List[Chunk]] = rep(emptyLine)~"```".r ^^ { _ => List() } | (textLine | emptyLine | emptyString)~_triple ^^ { case x~y => x +: y}
       """```""" ~ rep(emptyLine) ~> _triple} ^^ (lines => CodeBlockChunk(foldedString(lines)))
+  override lazy val hardBreakTextLine: Parser[Chunk] =
+    """[\t ]*\S[^\n]*[ ]{2}\r?\n""".r ^^ {
+      s => TextChunk(s)
+    }
+  /** A special case where an emphasis marker on a word on a text block doesn't
+      make the block a list item. */
+  override lazy val leadingStrongTextBlock: Parser[Chunk] =
+    """[ ]{0,3}\*\*[^*\n]+\*\*[^\n]*\n?""".r ~ rep(textLine) ^^ {
+      case ~(strLine, textSeq) => TextChunk(strLine + foldedString(textSeq))
+    }
+  override lazy val equalsLine: Parser[Any] = """=+\r?\n""".r
+  override lazy val dashesLine: Parser[Any] = """-+\r?\n""".r
+  override lazy val horizontalRule: Parser[Chunk] = {
+    val hrChar = """*\-_="""
+    s"""[ ]{0,3}[$hrChar][\t ]?[$hrChar][\t ]?[$hrChar][\t $hrChar]*\r?\n""".r ^^ {
+      s => HorizontalRuleChunk
+    }
+  }
+  override lazy val linkDefinition: Parser[Chunk] =
+    linkIDAndURL ~ opt(linkTitle) <~ """[ ]*(\r?\n)?""".r ^^ {
+      case ~(idAndURL, titleOpt) =>
+        LinkDefinitionChunk(idAndURL._1, idAndURL._2, titleOpt)
+    }
+  private def linkIDAndURL: Parser[(String, String)] =
+    """[ ]{0,3}\[[^\[\]]*\]:[ ]+<?[\w\p{Punct}]+>?""".r ^^ {
+      linkString =>
+        val linkMatch = """^\[([^\[\]]+)\]:[ ]+<?([\w\p{Punct}]+)>?$""".r
+          .findFirstMatchIn(linkString.trim).get;
+        (linkMatch.group(1), linkMatch.group(2))
+    }
 
+  private def linkTitle: Parser[String] =
+    """\s*""".r ~> """["'(].*["')]""".r ^^ (// " <- My TextMate bundle fails here
+      str => str.substring(1, str.length - 1))
   private def foldedString(texts: List[Chunk]): String =
       ("" /: texts)((current, text) => current + text.content)
 }
-
 case class CodeBlockChunk(val content: String) extends Chunk {
 
   def appendNewBlock(list: ListBuffer[Block],
